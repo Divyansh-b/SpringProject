bfe6e196069bac35544ab327c1d806a4e7a2bc26
Divyansh <divyanshbansal56@gmail.com>
2025-03-03 11:07:25 +0530

    Setup

---
+++ src/test/java/org/example/secondspringbootproject/SecondSpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.secondspringbootproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondSpringBootProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
