b79c5e4a5ec7094da1fdd499422c56d995fd8035
Divyansh <divyanshbansal56@gmail.com>
2025-03-03 11:20:38 +0530

    Added Dependencies

--- src/main/java/org/example/secondspringbootproject/SpringConceptsApplication.java
+++ src/main/java/org/example/secondspringbootproject/SpringConceptsApplication.java
@@ -1,5 +1,7 @@
 package org.example.secondspringbootproject;
 
+
+import org.example.secondspringbootproject.component.EmployeeBean;
 import org.example.secondspringbootproject.component.DemoBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,16 +10,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class SpringConceptsApplication {
-    public static final Logger logger = LoggerFactory.getLogger(SpringConceptsApplication.class);
+class SpringConceptsApplication {
+    private static final Logger logger = LoggerFactory.getLogger(SpringConceptsApplication.class);
+
     public static void main(String[] args) {
-        logger.debug ("Welcome to Spring Concept Demo");
         logger.info("Starting Spring Boot Application...");
-        ApplicationContext context = SpringApplication. run (SpringConceptsApplication.class, args);
-        DemoBean demoBean = context.getBean(DemoBean.class);
-        demoBean.sayHello();
-        logger.info("Application Started Successfully!");
-        logger.debug ("Checking Context: {}", context.getBean (DemoBean.class) );
-        logger.debug("\n*** Example Using @Autowire annotation on property ***");
+        ApplicationContext context = SpringApplication.run(SpringConceptsApplication.class, args);
+        logger.debug("Checking Context:{}",context.getBean(DemoBean.class));
+
+        logger.debug("\n*** Example using @Autowire annotation on property ***");
+        EmployeeBean employeeBean=context.getBean(EmployeeBean.class);
+        employeeBean.setEid(104);
+        employeeBean.setEname("Spring Framework Guru");
+        employeeBean.showEmployeeDetails();
     }
 }
